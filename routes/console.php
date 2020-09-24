9787d57174c8792abc0439b21e3e6a8ef3be734b
Swapnil Kumbhar <swapkumbhar31@gmail.com>
2020-09-24 19:08:04 +0530

    production command added

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('production', function () {
+    $this->comment('Setup For project');
+    $this->comment('-------------------------------------------------');
+    $this->comment('Installing Angular dependency');
+    exec('npm i');
+    $this->comment('Installed Angular dependency');
+    $this->comment('-------------------------------------------------');
+    $this->comment('Building Angular Project');
+    exec('ng build --prod');
+    $this->comment('Build Angular Project');
+    $this->comment('-------------------------------------------------');
+    $this->comment('Copying resource files');
+    exec('rm -rf public/*.js');
+    exec('rm -rf  public/*.css');
+    exec('rm -rf  public/*.ico');
+    exec('cp -R dist/* public/');
+    exec('rm public/index.html');
+    exec('cp dist/index.html resources/views/index.blade.php');
+    $this->comment('Copied resource files');
+});
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{any1?}/{any2?}/{any3?}/{any4?}/{any5?}', function () {
+    return \View::make('index');
 });
